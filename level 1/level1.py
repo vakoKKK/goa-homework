761887cc57d0991fb73384611bf646a71a965216
vakoKKK <vakokvara2020@gmail.com>
2024-05-24 14:46:59 +0400

    first commit

---
+++ level 1/level1.py
@@ -0,0 +1,6 @@
+name="chemi saxelia vako"
+surname="kvaratshkelia"
+age="var 15 wlis"
+height="chemi simaglea 177 santimetri"
+living_place="vcxovrob tbilishi"
+print(name + " " + surname + " " + str(age) + " " + str(height) + " " + profession + " " + living_place)
