c3e405f9a204b8aaf74259b38ac19d9dad7e1d3e
vakoKKK <vakokvara2020@gmail.com>
2024-06-02 13:37:02 +0400

    first commit

---
+++ level 5/level5.py
@@ -0,0 +1,49 @@
+# declared variables and gave it string type values.
+# used snake cases in variable names by using underscores. It creates spaces between variable names.
+# gave variable name: dog_name1 and value: "Rick"
+# started dogs names with uppercase letters that is right and looks better
+dog_name1 = "Rick"
+# variable name is dog_name2 value is "John"
+dog_name2 = "John"
+# reassigned dog_name2's value from "John" to "Max"
+dog_name2 = "Max"
+
+# print command is used to display a message on the screen. The message can be any type.
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ".")
+#              |               
+#     this is str type text   " " is used to create space between words
+
+
+#TASK 2
+
+
+# F string
+# 1) 
+name = "Giorgi"
+age = 14
+school = "GOAL ORIENTED ACADEMY"
+print(f"1)  my name is {name}. i am {age} years old and im studying at {school}.")
+
+
+# 2)
+text = "Goaland"
+time = "1st june"
+print(f"2)  {text} is in {time} and i want to go there but my schools's excursion is also in {time} so i donk know what to do '_' ")
+
+
+# 3)
+name = "Goal oriented academy"
+name2 = "GOA"
+text = "programming"
+time = 2.5
+text2 = "pyhon html css and many more programming lanuages"
+print(f"3)  {name} is best academy ITW and im gonna learn {text} there. in {name2} there is {time} year courses and in that time im gonna learn {text2}.")
+
+
+# 4)
+language =  "Python"
+name = "Guido van Rossum"
+country = "Dutch"
+time = "20th February 1991"
+advantage = "developing websites and software, task automation, data analysis and data visualisation"
+print(f"4)  {language} was created by {name} in {time}. {name} was {country} programmer. python is good for
