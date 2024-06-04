1742c0b8f1dd81c60d1573698857657e2d0eb774
vakoKKK <vakokvara2020@gmail.com>
2024-06-04 23:13:43 +0400

    first commit

---
+++ level 6/level6.py
@@ -0,0 +1,14 @@
+#შემოვიტანოთ ცვლადები
+deda=40
+mama=40
+me=15
+bebo=65
+babu=70
+
+
+#დავწეროთ პრინტ ფუნქცია მათემატიკური გამოთვლით
+print(deda + year)
+print(mama + year)
+print(me + year)
+print(bebo + year)
+print(babu + year)
