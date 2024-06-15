34de917aaee29df15d22c3a7ad054fa8e2b6ea78
vakoKKK <vakokvara2020@gmail.com>
2024-06-15 23:58:38 +0400

    first commit

--- level 6/level6.py
+++ level 6/level6.py
@@ -1,14 +1,31 @@
-#შემოვიტანოთ ცვლადები
-deda=40
-mama=40
-me=15
-bebo=65
-babu=70
+num1=1
+num2=2
+print(num1+num2)
+print(num2-num1)
+print(num1*num2)
+print(num2/num1)
 
+#დავალება 2
 
-#დავწეროთ პრინტ ფუნქცია მათემატიკური გამოთვლით
-print(deda + year)
-print(mama + year)
-print(me + year)
-print(bebo + year)
-print(babu + year)
+print(input("please enter your name: "))
+print("happy birthday")
+
+#დავალება 3
+
+print(input("please enter your full name: "))
+print(input("please enter your age: "))
+print(input("please enter password: "))
+
+#დავალება 4
+
+წევრი1=11
+წევრი2=13
+წევრი3=15
+წევრი4=36
+წევრი5=38
+
+print(წევრი1+25)
+print(წევრი2+25)
+print(წევრი3+25)
+print(წევრი4+25)
+print(წევრი5+25)
